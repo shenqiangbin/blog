579714309d6693df46fc0d9ef862103c38ef5405
shenqiangbin <shenqiangbin@163.com>
2017-09-06 00:26:30 +0800

    add

---
+++ Blog/Areas/backmgr/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Areas.backmgr.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: backmgr/Home
+        public ActionResult Index()
+        {
+            //return Content("backmgr");
+           return View();
+        }
+    }
+}
