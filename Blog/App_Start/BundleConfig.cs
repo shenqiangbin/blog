18eba8ac9a79a9cd8292d2343d691edecd9255ab
shenqiangbin <shenqiangbin@163.com>
2017-09-06 01:29:07 +0800

    完成新增博客的视图

--- Blog/App_Start/BundleConfig.cs
+++ Blog/App_Start/BundleConfig.cs
@@ -15,6 +15,17 @@ namespace Blog.App_Start
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
             bundles.Add(new StyleBundle("~/Content/index").Include("~/Content/index.css"));
+
+
+            bundles.Add(new ScriptBundle("~/Scripts/ueditor/js").Include(
+                        "~/Scripts/ueditor/ueditor.config.js",
+                        "~/Scripts/ueditor/ueditor.all.min.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/Scripts/ueditor/lang/zh-cn/zh").Include(
+                        "~/Scripts/ueditor/lang/zh-cn/zh-cn.js"
+                        ));
+
         }
     }
 }
---
+++ Blog/Areas/backmgr/Controllers/ArticleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Areas.backmgr.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: backmgr/Article
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
