5fe7bf244aac56c7b696174d110c673ad1c093e2
shenqiangbin <shenqiangbin@163.com>
2017-09-26 20:30:46 +0800

    合并

---
+++ Blog/Areas/backmgr/Controllers/LabelController.cs
@@ -0,0 +1,29 @@
+﻿using Blog.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Areas.backmgr.Controllers
+{
+    public class LabelController : Controller
+    {
+        private LabelRepository _labelRepository;
+
+        public LabelController(LabelRepository labelRepository)
+        {
+            _labelRepository = labelRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
