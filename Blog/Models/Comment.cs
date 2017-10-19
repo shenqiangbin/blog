71827a23c76e2ba675ffd44ff6c0d49da42ed7ef
shenqiangbin <shenqiangbin@163.com>
2017-10-19 23:57:14 +0800

    后台添加评论管理

--- Blog/Models/Comment.cs
+++ Blog/Models/Comment.cs
@@ -19,4 +19,20 @@ namespace Blog.Models
         /*扩展属性*/
         public IEnumerable<Comment> Children { get; set; }
     }
+
+    /*后台评论管理使用*/
+    public class CommentInfo
+    {
+        public int CommentId { get; set; }
+        public int ArticleId { get; set; }
+        public string UserName { get; set; }
+        public string Content { get; set; }
+        public int ParentId { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public int Enable { get; set; }
+
+        /*扩展属性*/
+        public string ArticleTitle { get; set; }
+    }
 }
