7836c8c8ad4b17414c4a045bf8088c7fe571569c
win10 <win10@DESKTOP-1OMAAB3>
2018-11-30 01:13:13 +0800

    改进富文本编辑器 - 页面使用editor.md

--- Blog/Scripts/backcommon.js
+++ Blog/Scripts/backcommon.js
@@ -19,3 +19,33 @@ var msger = {
         layer.msg(msg);
     }
 }
+
+function removejscssfile(filename, filetype) {
+    debugger;
+    var targetelement = (filetype == "js") ? "script" : (filetype == "css") ? "link" : "none";
+    var targetattr = (filetype == "js") ? "src" : (filetype == "css") ? "href" : "none";
+    var allsuspects = document.getElementsByTagName(targetelement);
+    for (var i = allsuspects.length; i >= 0; i--) {
+        
+        if (allsuspects[i] && allsuspects[i].getAttribute(targetattr) != null && allsuspects[i].getAttribute(targetattr).indexOf(filename) != -1)
+            allsuspects[i].parentNode.removeChild(allsuspects[i]);
+    }
+
+}
+
+function loadjscssfile(filename, filetype) {
+    if (filetype == "js") {
+        var fileref = document.createElement('script');
+        fileref.setAttribute("type", "text/javascript");
+        fileref.setAttribute("src", filename);
+    }
+    else if (filetype == "css") {
+        var fileref = document.createElement("link");
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", filename);
+    }
+
+    if (typeof fileref != "undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref);
+}
