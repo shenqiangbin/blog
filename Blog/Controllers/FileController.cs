67828c752803b86674866a4f8dcaa005dbba9205
win10 <win10@DESKTOP-1OMAAB3>
2018-12-02 13:46:53 +0800

    改进富文本编辑器 - 图片上传

--- Blog/Controllers/FileController.cs
+++ Blog/Controllers/FileController.cs
@@ -30,12 +30,18 @@ namespace Blog.Controllers
                 return Ok(new { success = 0, message = "文件大于请小于20M" });
             }
 
-            string theFile = Path.GetFileName(MD5Helper.MD5Value(file.FileName));
-            var fileName = Path.Combine(HttpContext.Current.Request.MapPath("~/Upload"), theFile);
+            string extension = file.FileName.Substring(file.FileName.LastIndexOf("."));
+            string theFile = Path.GetFileName(MD5Helper.MD5Value(file.FileName))+extension;
+
+            string folderPath = HttpContext.Current.Request.MapPath("~/Upload");
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            var fileName = Path.Combine(folderPath, theFile);
             try
             {
                 file.SaveAs(fileName);
-                string path = "../upload/" + fileName;
+                string path = "/upload/" + theFile;
                 return Ok(new { success = 1, message = "", url = path });
             }
             catch (Exception ex)
@@ -47,14 +53,17 @@ namespace Blog.Controllers
         private bool CheckFileType(HttpPostedFile file)
         {
             string extension = file.FileName.Substring(file.FileName.LastIndexOf("."));
-            List<string> extens = new List<string>() { "jpg", "jpeg", "gif", "png", "bmp" };
+            List<string> extens = new List<string>() { ".jpg", ".jpeg", ".gif", ".png", ".bmp" };
             return extens.Contains(extension);
         }
 
         private bool CheckFileSize(HttpPostedFile file)
         {
             var size = file.ContentLength / 1024 / 1024;
-            return size > 200; //如果大于20M
+            if (size > 200) //如果大于20M
+                return false;
+            else
+                return true;
         }
     }
 }
