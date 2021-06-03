d5c1a9d956be120c7b8e6f53c8200001891cd9af
SQB10190 <sqb10190@cnki.net>
2021-06-04 00:20:41 +0800

    添加 404 页面 和 500 页面

---
+++ Blog/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Eoor
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        public ActionResult HasError()
+        {
+            return View();
+        }
+    }
+}
